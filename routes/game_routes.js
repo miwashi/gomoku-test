cd8aa67617589f3a0eb86f622dfc4b940bfccf07
Mikael Wallin <mikael.x.wallin@gmail.com>
2023-06-08 13:24:53 +0200

    Working swagger

--- routes/game_routes.js
+++ routes/game_routes.js
@@ -1,17 +1,76 @@
 const router = require('express').Router();
 const gameController = require('../controllers/game_controller.js')
 
-
+/**
+ * @swagger
+ * /api/game/games:
+ *   get:
+ *     description: Use login api
+ *
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ */
 router.get('/games', gameController.getGames);
 
+/**
+ * @swagger
+ * /api/game/games/{id}:
+ *   get:
+ *     description: Use login api
+ *
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ */
 router.get('/games/:id', gameController.findGameById);
 
+/**
+ * @swagger
+ * /api/game/games/add:
+ *   get:
+ *     description: Use login api
+ *
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ */
 router.get('/games/add', gameController.createGame);
 
+/**
+ * @swagger
+ * /api/game/player/join/{gameId}/{playerId}:
+ *   get:
+ *     description: Use login api
+ *
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ */
 router.get('/player/join/:game/:player', gameController.joinGame);
 
+/**
+ * @swagger
+ * /api/game/player/play/{gameId}/{playerId}/{row}/{col}:
+ *   get:
+ *     description: Use login api
+ *
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ */
 router.get('/player/play/:game/:player/:col/:row', gameController.play);
 
+/**
+ * @swagger
+ * /api/game/player/create:
+ *   get:
+ *     description: Use login api
+ *
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ */
 router.get('/player/create/', gameController.createPlayer);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const cors = require('cors')
 var favicon = require('serve-favicon');
 var path = require('path');
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
 
 const app = express();
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(express.static('public'));
 
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            version: "1.0.0",
+            title: "Gomoku API",
+            description: "API Information of gomoku-backend",
+            contact: {
+                name: "Wacoco"
+            },
+            servers: ["http://localhost:3000"]
+        }
+    },
+    apis: ['./routes/*.js']
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use('/api/game', require('./routes/game_routes.js'));
 
