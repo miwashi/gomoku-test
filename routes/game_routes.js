a1d5abb21e0945043274c933cb6ddd231a8088f7
Mikael Wallin <mikael.x.wallin@gmail.com>
2023-09-27 16:11:02 +0200

    added swagger documentation

--- routes/game_routes.js
+++ routes/game_routes.js
@@ -38,6 +38,13 @@ router.get('/games', gameController.getGames);
  *         required: true
  *         type: string
  *         format: uuid
+ *         #enum:
+ *         #  - "015cdc04-4d22-46f7-8d8e-f1879bb9bf1b"
+ *         #  - "88aaf28d-7fef-4028-9e94-7fdbbbd662a0"
+ *         #  - "6d3e9d43-9d19-4fb5-a684-b8514f0e4810"
+ *         #  - "ebdf150a-4740-42ea-a5a7-dfce2f6f3725"
+ *         #  - "6d8fbefa-c1ed-4401-b7b1-894581d84798"
+ *         #  - "a451aa61-76f0-4cd0-bb37-8594512d7bde"
  *     responses:
  *       200:
  *         description: A game object
@@ -52,25 +59,56 @@ router.get('/games/:id', gameController.findGameById);
 
 /**
  * @swagger
- * /api/game/games/add:
+ * /games/add:
  *   get:
- *     description: Use login api
- *
+ *     tags:
+ *       - Games
+ *     description: Adds a new game with a name
  *     responses:
- *       '200':
- *         description: A successful response
+ *       200:
+ *         description: Game successfully added
+ *         schema:
+ *           $ref: '#/definitions/Game'
+ *       400:
+ *         description: Bad request (e.g., missing name data)
+ *       500:
+ *         description: Server error
  */
 router.get('/games/add', gameController.createGame);
 
 /**
  * @swagger
- * /api/game/player/join/{gameId}/{playerId}:
- *   get:
- *     description: Use login api
- *
+ * /player/join/{game}/{player}:
+ *   get:   # Again, consider changing this to PUT or POST as recommended earlier.
+ *     tags:
+ *       - Players
+ *     description: Allows a player to join a game
+ *     parameters:
+ *       - in: path
+ *         name: game
+ *         description: ID of the game to join
+ *         required: true
+ *         type: string
+ *         format: uuid
+ *       - in: path
+ *         name: player
+ *         description: User ID of the player joining the game
+ *         required: true
+ *         type: string
+ *         format: uuid
  *     responses:
- *       '200':
- *         description: A successful response
+ *       200:
+ *         description: Successfully joined the game
+ *         schema:
+ *           $ref: '#/definitions/Game'
+ *       400:
+ *         description: Bad request (e.g., invalid game or player ID)
+ *       403:   # or 409 depending on your preference
+ *         description: Game is already full
+ *       404:
+ *         description: Game or player not found
+ *       500:
+ *         description: Server error
  */
 router.get('/player/join/:game/:player', gameController.joinGame);
 
@@ -144,14 +182,18 @@ module.exports = router
  *       rows:
  *         type: integer
  *         default: 16
- *       squares:
- *         type: array
- *         items:
- *           type: array
- *           items:
- *             type: integer
- *         minItems: 16
- *         maxItems: 16
+ *       tiles:
+ *         $ref: '#/definitions/Tiles'
+ *   Tiles:
+ *     type: array
+ *     items:
+ *       type: array
+ *       items:
+ *         type: integer
+ *       minItems: 5
+ *       maxItems: 16
+ *     minItems: 5
+ *     maxItems: 16
  *   User:
  *     type: object
  *     required:
